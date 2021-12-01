c5eb515b99044fd73838b4a31ac8b891b30aea6f
atkins <satkins@simondata.com>
2021-12-01 15:33:52 -0700

    Hooking up socket

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const chatSocket = new WebSocket("ws://localhost:8000/ws/test/");
 
 function App() {
+  const [song, setSong] = useState<string>();
+  useEffect(() => {
+    chatSocket.onopen = () => {
+      console.log("WebSocket Client Connected");
+      chatSocket.send(JSON.stringify({ type: "getSong" }));
+    };
+
+    chatSocket.onmessage = (message) => {
+      const jsonData = JSON.parse(message.data);
+      if (jsonData.song !== undefined) {
+        setSong(jsonData.song);
+      } else {
+        console.error("no song found!");
+      }
+    };
+
+    return function cleanup() {
+      chatSocket.close();
+    };
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Song: {song}</p>
       </header>
     </div>
   );
--- sing_along_api/asgi.py
+++ sing_along_api/asgi.py
@@ -9,14 +9,18 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+import sing_along_api.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sing_along_api.settings")
 
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
-        # Just HTTP for now. (We can add other protocols later.)
+        "websocket": AuthMiddlewareStack(
+            URLRouter(sing_along_api.routing.websocket_urlpatterns)
+        ),
     }
 )
