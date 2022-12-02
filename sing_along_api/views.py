d2c3d387c6e3dca2d4d16e193edc2edc64ef5028
Sam Atkins <sam@trufflesec.com>
2022-12-02 16:14:53 -0700

    Remove dead code, add black

--- sing_along_api/views.py
+++ sing_along_api/views.py
@@ -6,44 +6,5 @@ from django.template import engines
 from django.views.generic import TemplateView
 
 
-def iter_response(response, chunk_size=65536):
-    try:
-        while True:
-            data = response.read(chunk_size)
-            if not data:
-                break
-            yield data
-    finally:
-        response.close()
-
-
-def catchall_dev(request, upstream="http://localhost:3000"):
-    upstream_url = upstream + request.path
-    response = urllib.request.urlopen(upstream_url)
-    content_type = response.getheader("Content-Type")
-
-    if content_type == "text/html; charset=UTF-8":
-        response_text = response.read().decode()
-        response.close()
-        return HttpResponse(
-            engines["django"].from_string(response_text).render(),
-            content_type=content_type,
-            status=response.status,
-            reason=response.reason,
-        )
-    else:
-        return StreamingHttpResponse(
-            iter_response(response),
-            content_type=content_type,
-            status=response.status,
-            reason=response.reason,
-        )
-
-
 def test_view(request):
     return JsonResponse({"msg": "hi", "debug": settings.DEBUG})
-
-
-catchall_prod = TemplateView.as_view(template_name="index.html")
-
-catchall = catchall_dev if settings.DEBUG else catchall_prod
