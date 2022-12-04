46c0b8411f0d3f63a4833c3a02fce521df8b65da
jackconwayiv <jackconwayiv@gmail.com>
2022-12-04 15:48:56 -0700

    Add React Router, favicon v0.1, browser title.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AddSongView from "./components/AddSongView";
 import CurrentSongView from "./components/CurrentSongView";
 
 function App() {
-  return <CurrentSongView />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<CurrentSongView />} />
+        <Route path="addSong" element={<AddSongView />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
