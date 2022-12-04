3397e690d6954ecdddbb2f381d2189539f587266
Sam Atkins <sam@trufflesec.com>
2022-12-04 09:48:38 -0700

    Refine db model

--- api/models.py
+++ api/models.py
@@ -12,7 +12,7 @@ class Songbook(SafeDeleteModel, CreatedUpdatedMixin):
     session_key = models.CharField(max_length=20, unique=True, null=False, blank=False)
     current_song_timestamp = models.DateTimeField(null=False, blank=False)
     max_active_songs = models.IntegerField(null=True, blank=True)
-    title = models.CharField(max_length=40, unique=True, null=False, blank=False)
+    title = models.CharField(max_length=40, null=False, blank=False)
     last_nav_action_taken_at = models.DateTimeField(null=True, blank=True)
     is_noodle_mode = models.BooleanField(null=False, blank=True, default=False)
 
@@ -28,9 +28,9 @@ class Song(SafeDeleteModel, CreatedUpdatedMixin):
             models.Index(fields=["title"]),
         ]
 
-    artist = models.CharField(max_length=40, null=False, blank=False, db_index=True)
+    artist = models.CharField(max_length=40, null=False, blank=False)
     title = models.CharField(max_length=40, null=False, blank=False)
-    url = models.CharField(max_length=400, null=False, blank=False)
+    url = models.TextField(null=False, blank=False)
     content = models.TextField(null=False, blank=False)
 
 
