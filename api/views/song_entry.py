4669b7b3dd6f7da02461ced500fcbb3bb688b2fe
Drew LaFiandra <79876588+alafiand@users.noreply.github.com>
2022-12-06 10:29:55 -0700

    updated comment in song_entry.py

--- api/views/song_entry.py
+++ api/views/song_entry.py
@@ -6,7 +6,7 @@ from api.serializers.song_entry import SongEntrySerializer
 
 class SongEntryViewSet(viewsets.ModelViewSet):
     """
-    API endpoint that allows all standard interactions with Songs.
+    API endpoint that allows all standard interactions with SongEntrys.
     """
 
     queryset = SongEntry.objects.all()
