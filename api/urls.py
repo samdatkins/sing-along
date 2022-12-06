52fe2cbdfeae7fb7571dfce15a3bc6bb683a3dc6
Drew LaFiandra <79876588+alafiand@users.noreply.github.com>
2022-12-06 09:59:57 -0700

    song_entry added

--- api/urls.py
+++ api/urls.py
@@ -2,9 +2,11 @@ from django.urls import path, re_path
 from rest_framework import routers
 
 from api.views.song import SongViewSet
+from api.views.song_entry import SongEntryViewSet
 
 router = routers.DefaultRouter()
 
 router.register(r"songs", SongViewSet)
+router.register(r"song_entry", SongEntryViewSet)
 
 urlpatterns = router.urls
---
+++ api/views/song_entry.py
@@ -0,0 +1,13 @@
+from rest_framework import viewsets
+
+from api.models import SongEntry
+from api.serializers.song_entry import SongEntrySerializer
+
+
+class SongEntryViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows all standard interactions with Songs.
+    """
+
+    queryset = SongEntry.objects.all()
+    serializer_class = SongEntrySerializer
