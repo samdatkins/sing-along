d540d0e36684ed208e5d4e3c001812c5c336dc0c
atkins <sam@trufflesec.com>
2022-12-09 18:15:56 -0700

    All endpoints except add (#11)
    * All endpoints except add
    
    * Fix naming

--- api/serializers/songbook.py
+++ api/serializers/songbook.py
@@ -1,9 +1,31 @@
 from rest_framework import serializers
 
 from api.models import Songbook
+from api.serializers.song import SongSerializer
 
 
 class SongbookSerializer(serializers.ModelSerializer):
+    total_songs = serializers.SerializerMethodField()
+    current_song_index = serializers.SerializerMethodField()
+    current_song_entry = serializers.SerializerMethodField()
+
     class Meta:
         model = Songbook
-        fields = "__all__"
+        fields = [
+            "session_key",
+            "max_active_songs",
+            "title",
+            "is_noodle_mode",
+            "total_songs",
+            "current_song_index",
+            "current_song_entry",
+        ]
+
+    def get_total_songs(self, obj):
+        return obj.get_total_song_count()
+
+    def get_current_song_index(self, obj):
+        return obj.get_current_song_index()
+
+    def get_current_song_entry(self, obj):
+        return SongSerializer(obj.get_current_song_entry().song).data
