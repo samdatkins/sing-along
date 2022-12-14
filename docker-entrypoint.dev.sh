e528c0d75b9cbe637cdcc41dbfee9c700a7db433
Sam Atkins <sam@trufflesec.com>
2022-12-14 15:38:54 -0700

    Integrate tests with CI

--- docker-entrypoint.dev.sh
+++ docker-entrypoint.dev.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# Collect static files
 echo "Collect static files"
 python manage.py collectstatic --noinput
 
-# Apply database migrations
 echo "Apply database migrations"
 python manage.py migrate
 
-# Start server
 echo "Starting dev server"
 DJANGO_ENV=development python manage.py runserver 0.0.0.0:8080
