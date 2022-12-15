5d7e43fbc58bdb3972140724487b5a6a5e1c4d36
Sam Atkins <sam@trufflesec.com>
2022-12-15 07:13:04 -0700

    Fix psql check

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+until psql "postgresql://postgres:postgres@db:5432/postgres" -c '\l'; do
+  echo >&2 "postgres is unavailable - sleeping"
+  sleep 1
+done
+
 if [ "${YOUR_ENV}" == "production" ]; then
   ./scripts/docker-entrypoint.prod.sh
 else
