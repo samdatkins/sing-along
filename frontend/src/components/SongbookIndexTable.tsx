84f0131fe78826d8a8fae05440c763f7ecf595ab
jackconwayiv <jackconwayiv@gmail.com>
2023-01-03 09:21:30 -0700

    Add tabbed table of songbooks to home page.

---
+++ frontend/src/components/SongbookIndexTable.tsx
@@ -0,0 +1,23 @@
+import { ListItem, Tab, TabList, TabPanel, TabPanels, Tabs, UnorderedList } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
+export default function SongbookIndexTable({songbooks}) {
+     return (  <Tabs>
+      <TabList>
+        <Tab>Power Hour</Tab>
+        <Tab>Noodle Mode</Tab>
+      </TabList>
+      <TabPanels>
+        <TabPanel
+        ><UnorderedList>{songbooks &&
+songbooks.filter(({ is_noodle_mode }) => !is_noodle_mode).map(({ title, session_key }) => (
+    <ListItem><Link to={`/live/${session_key}/`}>{title}</Link></ListItem>
+))}</UnorderedList></TabPanel>
+          <TabPanel><UnorderedList>{songbooks &&
+songbooks.filter(({ is_noodle_mode }) => is_noodle_mode).map(({ title, session_key }) => (
+    <ListItem><Link to={`/live/${session_key}/`}>{title}</Link></ListItem>
+))}</UnorderedList>
+          </TabPanel>
+      </TabPanels>
+    </Tabs>);
+  }
