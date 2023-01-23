2fcb79f78fd30e94733024899979f360a4b39c1a
Sam Atkins <sam@trufflesec.com>
2023-01-22 17:42:58 -0700

    Add rating and votes fields

--- api/models.py
+++ api/models.py
@@ -114,6 +114,8 @@ class Song(SafeDeleteModel, CreatedUpdated):
     title = models.CharField(max_length=120, null=False, blank=False)
     url = models.TextField(null=False, blank=False)
     content = models.TextField(null=True, blank=True)
+    rating = models.DecimalField(max_digits=8, decimal_places=6, null=True, blank=True)
+    votes = models.IntegerField(null=True, blank=True)
 
 
 class SongEntry(SafeDeleteModel, CreatedUpdated):
