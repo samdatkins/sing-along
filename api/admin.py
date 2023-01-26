1208dff966572c6d199d10e9fc5747654d6b046d
Sam Atkins <sam@trufflesec.com>
2023-01-26 13:35:19 -0700

    Make django admin not suck

--- api/admin.py
+++ api/admin.py
@@ -2,6 +2,53 @@ from django.contrib import admin
 
 from .models import Song, Songbook, SongEntry
 
-admin.site.register(Songbook)
-admin.site.register(Song)
-admin.site.register(SongEntry)
+
+class SongAdmin(admin.ModelAdmin):
+    list_display = ("artist", "title", "url", "rating", "votes", "capo")
+    search_fields = ("artist", "title", "url", "capo")
+
+
+class SongbookAdmin(admin.ModelAdmin):
+    list_display = ("session_key", "title", "is_noodle_mode")
+    search_fields = ("session_key", "title")
+
+
+class SongEntryAdmin(admin.ModelAdmin):
+    list_select_related = ("song", "songbook")
+    list_display = (
+        "get_songbook_title",
+        "get_song_title",
+        "get_song_artist",
+        "play_time",
+    )
+    search_fields = (
+        "songbook__title",
+        "song__title",
+        "song__artist",
+    )
+
+    @admin.display(
+        ordering="songbook__title",
+        description="Songbook Title",
+    )
+    def get_songbook_title(self, obj):
+        return obj.songbook.title
+
+    @admin.display(
+        ordering="song__title",
+        description="Song Title",
+    )
+    def get_song_title(self, obj):
+        return obj.song.title
+
+    @admin.display(
+        ordering="song__artist",
+        description="Song Artist",
+    )
+    def get_song_artist(self, obj):
+        return obj.song.artist
+
+
+admin.site.register(Songbook, SongbookAdmin)
+admin.site.register(Song, SongAdmin)
+admin.site.register(SongEntry, SongEntryAdmin)
