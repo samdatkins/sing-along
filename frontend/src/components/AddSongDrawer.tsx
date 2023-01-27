5a9a5e9b814ea6f9d4a1989298f4ac7a4b850f40
Sam Atkins <sam@trufflesec.com>
2023-01-26 22:23:56 -0700

    Remove form from add song drawer

--- frontend/src/components/AddSongDrawer.tsx
+++ frontend/src/components/AddSongDrawer.tsx
@@ -49,59 +49,57 @@ export default function AddSongDrawer() {
           <DrawerHeader>Request a song for "{songbook?.title}"</DrawerHeader>
 
           <DrawerBody>
-            <form>
-              <SongSearchAutocomplete
-                onSubmit={async (song) => {
-                  setAlertText("");
-                  setAlertStatus(undefined);
+            <SongSearchAutocomplete
+              onSubmit={async (song) => {
+                setAlertText("");
+                setAlertStatus(undefined);
 
-                  const addSongResult = await addSongToSongbook(
-                    song,
-                    songbook?.id
-                  );
-                  if (typeof addSongResult === "string") {
-                    const isWarning = addSongResult.includes("already");
-                    setAlertStatus(isWarning ? "warning" : "error");
-                    setAlertText(addSongResult);
-                    if (isWarning) {
-                      return true;
-                    }
-                    return false;
-                  } else {
-                    setUndoSongEntryID(addSongResult.data.id);
-                    setAlertStatus("success");
-                    setAlertText(
-                      `Successfully added "${song.title}" by ${song.artist}.`
-                    );
+                const addSongResult = await addSongToSongbook(
+                  song,
+                  songbook?.id
+                );
+                if (typeof addSongResult === "string") {
+                  const isWarning = addSongResult.includes("already");
+                  setAlertStatus(isWarning ? "warning" : "error");
+                  setAlertText(addSongResult);
+                  if (isWarning) {
                     return true;
                   }
-                }}
-              />
+                  return false;
+                } else {
+                  setUndoSongEntryID(addSongResult.data.id);
+                  setAlertStatus("success");
+                  setAlertText(
+                    `Successfully added "${song.title}" by ${song.artist}.`
+                  );
+                  return true;
+                }
+              }}
+            />
 
-              <SlideFade in={!!alertText} style={{ zIndex: 10 }} offsetY="20px">
-                <Alert status={alertStatus} py="2rem" rounded="md" mt="3rem">
-                  <Flex direction="column">
-                    <Flex direction="row" alignItems="center">
-                      <AlertIcon />
-                      <AlertDescription>{alertText}</AlertDescription>
-                    </Flex>
-                    {undoSongEntryID !== undefined && (
-                      <Button
-                        mt="1rem"
-                        onClick={() => {
-                          deleteSongbookSong(undoSongEntryID);
-                          setUndoSongEntryID(undefined);
-                          setAlertText("");
-                          setAlertStatus(undefined);
-                        }}
-                      >
-                        Undo
-                      </Button>
-                    )}
+            <SlideFade in={!!alertText} style={{ zIndex: 10 }} offsetY="20px">
+              <Alert status={alertStatus} py="2rem" rounded="md" mt="3rem">
+                <Flex direction="column">
+                  <Flex direction="row" alignItems="center">
+                    <AlertIcon />
+                    <AlertDescription>{alertText}</AlertDescription>
                   </Flex>
-                </Alert>
-              </SlideFade>
-            </form>
+                  {undoSongEntryID !== undefined && (
+                    <Button
+                      mt="1rem"
+                      onClick={() => {
+                        deleteSongbookSong(undoSongEntryID);
+                        setUndoSongEntryID(undefined);
+                        setAlertText("");
+                        setAlertStatus(undefined);
+                      }}
+                    >
+                      Undo
+                    </Button>
+                  )}
+                </Flex>
+              </Alert>
+            </SlideFade>
           </DrawerBody>
 
           <DrawerFooter></DrawerFooter>
