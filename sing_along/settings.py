f69f80e02f7f74c3eac2be98b80f011029424367
Sam Atkins <sam@trufflesec.com>
2023-01-29 10:03:23 -0700

    Remove DRF default auth scheme while testing

--- sing_along/settings.py
+++ sing_along/settings.py
@@ -179,9 +179,7 @@ REST_FRAMEWORK = {
         "rest_framework.renderers.JSONRenderer",
         "rest_framework.renderers.BrowsableAPIRenderer",
     ],
-    "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.permissions.IsAuthenticated",
-    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [],
     "DEFAULT_PERMISSION_CLASSES": [],
     "PAGE_SIZE": 100,
 }
