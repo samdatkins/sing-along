9ad42889898a7e556ff4fd3e743c43e12ba3828f
Sam Atkins <sam@trufflesec.com>
2023-05-27 08:49:18 -0700

    More tests

--- api/tests/views/test_user.py
+++ api/tests/views/test_user.py
@@ -1,4 +1,5 @@
 import factory.random
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework.test import APIRequestFactory, force_authenticate
@@ -39,3 +40,39 @@ class TestUser(TestCase):
 
         # Assert
         self.assertEqual(response.status_code, 403)
+
+    def test_edit_succeeds(self):
+        # Arrange
+        new_first_name = "my brand new first name"
+        api_factory = APIRequestFactory()
+        view = UserView.as_view()
+        request = api_factory.patch(
+            reverse("user-detail"), {"first_name": new_first_name}
+        )
+        force_authenticate(request, user=self.user)
+
+        # Act
+        response = view(request)
+
+        # Assert
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["first_name"], new_first_name)
+
+        edited_user = get_user_model().objects.get(pk=self.user.pk)
+        self.assertEqual(edited_user.first_name, new_first_name)
+
+    def test_edit_id_is_ignored(self):
+        # Arrange
+        new_id = 9999999
+        old_id = self.user.pk
+        api_factory = APIRequestFactory()
+        view = UserView.as_view()
+        request = api_factory.patch(reverse("user-detail"), {"id": new_id})
+        force_authenticate(request, user=self.user)
+
+        # Act
+        response = view(request)
+
+        # Assert
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["id"], old_id)
