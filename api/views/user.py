428e3e7881ae94a4513ccf0bec16dee9b0d12468
jackconwayiv <jackconwayiv@gmail.com>
2023-06-03 14:10:12 -0700

    add migration for user_profile table

--- api/views/user.py
+++ api/views/user.py
@@ -4,6 +4,7 @@ from django.contrib.auth import get_user_model
 from rest_framework import generics
 from social_django.models import UserSocialAuth
 
+from api.models import UserProfile
 from api.serializers.user import UserSerializer
 
 logger = logging.getLogger(__name__)
@@ -14,10 +15,16 @@ class UserView(generics.RetrieveUpdateAPIView):
     API endpoint that allows all standard interactions with Songs.
     """
 
-    queryset = get_user_model().objects.all()
+    queryset = get_user_model().objects.select_related("userprofile").all()
     serializer_class = UserSerializer
 
     def get_object(self):
         user = self.request.user
         user.social = UserSocialAuth.objects.get(user_id=user.pk)
+
+        try:
+            profile = user.userprofile
+        except UserProfile.DoesNotExist:
+            profile = UserProfile.objects.create(user=user)
+
         return user
