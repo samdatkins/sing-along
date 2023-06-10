27f8fbbb6522e52e17cfdcefb767fcb6516c3b4b
jackconwayiv <jackconwayiv@gmail.com>
2023-06-10 11:59:32 -0700

    restore the space-time continuum

--- frontend/src/models.ts
+++ frontend/src/models.ts
@@ -60,7 +60,7 @@ export enum ApplicationState {
 }
 
 export const AppStateToTimerMap = {
-  [ApplicationState.ShowSong]: 10,
+  [ApplicationState.ShowSong]: 60,
   [ApplicationState.ActionPrompt]: 7,
   [ApplicationState.PrepForSong]: 8,
 };
